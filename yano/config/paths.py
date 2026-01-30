f9593b5a33cddca22894852048d069f4cd43852d
kathleenabadie <kathleen.abadie@alleninstitute.org>
2026-01-29 18:59:17 -0800

    yano reprocessing

--- yano/config/paths.py
+++ yano/config/paths.py
@@ -10,7 +10,9 @@ from pathlib import Path
 REPO_DIR = Path(__file__).resolve().parents[1]
 
 # CHANGE THIS to your local data directory
-BASE_DIR = Path("/home/workspace/projects/drg")
+BASE_DIR = Path("/home/workspace/DRG/spatial_mouse_drg_outputs/yano") # KA update
+# BASE_DIR = Path("/home/workspace/projects/drg") # MC previously
+
 
 # Metadata containing manual annotations, housed within repo
 METADATA_DIR = REPO_DIR / "metadata"
