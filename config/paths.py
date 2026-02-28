602101adea1b8de8c986dbf7a705a09fb93038c0
kathleenabadie <kathleen.abadie@alleninstitute.org>
2026-02-28 15:28:26 -0800

    org

--- config/paths.py
+++ config/paths.py
@@ -1,3 +1,8 @@
+#### KA updating 20260228
+
+
+
+
 # config/paths.py
 """
 Path configuration for the DRG Xenium analysis project.
@@ -7,7 +12,14 @@ Edit BASE_DIR to the location of local data directory.
 
 from pathlib import Path
 
-REPO_DIR = Path(__file__).resolve().parents[1]
+# CHANGE THIS to your local data directory for intermediate outputs
+DATA_DIR = Path("/home/workspace/data/temp/DRG")
 
-# CHANGE THIS to your local data directory
-BASE_DIR = Path("/home/workspace/projects/drg")
+# CHANGE THIS to your local data directory where raw xenium data is stored
+BASE_DIR = Path("/home/workspace/DRG/spatial_mouse_drg_outputs/Kim")
+# BASE_DIR = Path("/home/workspace/projects/drg") # MC previous
+
+
+
+
+# REPO_DIR = Path(__file__).resolve().parents[1]
